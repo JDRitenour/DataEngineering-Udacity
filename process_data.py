9ce113ca9d564a383f80a2c46bb85ad83dbdc96d
Julie Ritenour <julieritenour@Julies-MacBook-Pro.local>
2022-11-30 18:52:50 -0500

    initial commit

---
+++ process_data.py
@@ -0,0 +1,40 @@
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+
+# load messages and categories datasets
+messages = pd.read_csv('messages.csv')
+
+categories = pd.read_csv('categories.csv')
+
+# merge datasets
+df = messages.merge(categories, on='id')
+
+# create a dataframe of the 36 individual category columns
+categories = df.categories.str.split(';', expand=True)
+
+# select the first row of the categories dataframe
+row = categories.iloc[0]
+category_colnames = row.apply(lambda x: x[:-2])
+
+# rename the columns of `categories`
+categories.columns = category_colnames
+
+for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = categories[column].apply(lambda x: x[-1])
+    # convert column from string to numeric
+    categories[column] = categories[column].astype('int64')
+
+# drop the original categories column from `df`
+df.drop(columns='categories', inplace=True)
+
+# concatenate the original dataframe with the new `categories` dataframe
+df = pd.concat([df, categories], axis=1, sort=False)
+
+# drop duplicates
+df.drop_duplicates(inplace=True)
+
+# load to sqlite database
+engine = create_engine('sqlite:///DisasterResponse.db')
+df.to_sql('DisasterResponse', engine, index=False)
---
+++ train.py
@@ -0,0 +1,93 @@
+# import libraries
+import re
+import pandas as pd
+from sqlalchemy import create_engine
+import pickle
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords', 'omw-1.4'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn import multioutput
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+
+
+def load_data():
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df = pd.read_sql_table('DisasterResponse', engine)
+    X = df['message']
+    y = df.iloc[:, 4:]
+    labels = list(df.columns[4:])
+    return X, y, labels
+
+
+def tokenize(text):
+    # remove special characters prior to tokenization
+    text_cleaned = re.sub('[^A-Za-z0-9]+', ' ', text.lower().strip())
+
+    # tokenize
+    tokens = word_tokenize(text_cleaned)
+
+    # define lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # define stop words
+    stop_words = stopwords.words('english')
+
+    # define list for cleaned tokens
+    clean_tokens = []
+
+    # define for loop to clean tokens
+    for tok in tokens:
+        if tok not in stop_words:
+            clean_tok = lemmatizer.lemmatize(tok)
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', multioutput.MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters = {
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__min_samples_split': [2, 3]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+
+def model_results(y_test, y_pred, labels):
+    for i in range(len(labels)):
+        print('Category: {} '.format(labels[i]))
+        print(classification_report(y_test.iloc[:, i].values, y_pred[:, i]))
+        print('Accuracy {}\n\n'.format(accuracy_score(y_test.iloc[:, i].values, y_pred[:, i])))
+
+
+def main():
+    X, y, labels = load_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=96)
+
+    model = build_model()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    model_results(y_test, y_pred, labels)
+
+    pickle.dump(model, open('model.pkl', 'wb'))
+
+
+main()
